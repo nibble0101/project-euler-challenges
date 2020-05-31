b944e188a346253104112e1b20c88ceb17e4c18d
nibble0101 <mj.mawa@yahoo.com>
2020-05-31 18:48:04 +0300

    Solved challenge 1

---
+++ index.js
@@ -0,0 +1,12 @@
+//Project Euler: Problem 1: Multiples of 3 and 5
+function multiplesOf3and5(number) {
+    let sum = 0;
+    for(let i = 3; i < number; i++){
+        if(i % 3 === 0 || i % 5 === 0){
+             sum += i;
+        }
+    }
+    return sum;
+  };
+  
+  
