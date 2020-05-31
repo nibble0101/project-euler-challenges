197ee569235a94bee360785df73f572edc0a3ada
nibble0101 <mj.mawa@yahoo.com>
2020-05-31 19:06:09 +0300

    Solved Challenge 2

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 //Project Euler: Problem 1: Multiples of 3 and 5
 function multiplesOf3and5(number) {
-    let sum = 0;
-    for(let i = 3; i < number; i++){
-        if(i % 3 === 0 || i % 5 === 0){
-             sum += i;
-        }
+  let sum = 0;
+  for (let i = 3; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
     }
+  }
+  return sum;
+}
+
+//Project Euler: Problem 2: Even Fibonacci Numbers
+function fiboEvenSum(n) {
+  let cache = [1, 2],
+    sum = 2;
+  if (n < 3) {
     return sum;
-  };
-  
-  
+  }
+  while (cache[cache.length - 1] <= n) {
+    const next = cache[cache.length - 1] + cache[cache.length - 2];
+    if (next % 2 === 0) {
+      sum += next;
+    }
+    cache.push(next);
+  }
+  return sum;
+}
